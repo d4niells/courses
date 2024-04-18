21127780f070f3c19ed49064962e53d92bd10a35
Daniel Oliveira <danieloliver.dev@gmail.com>
2024-04-18 14:57:28 -0300

    fuzzing

--- go-expert/testing/1/tax_test.go
+++ go-expert/testing/1/tax_test.go
@@ -39,3 +39,24 @@ func BenchmarkCalculateTax(b *testing.B) {
 		CalculateTax(500.0)
 	}
 }
+
+// go test -fuzz=. -fuzztime=3s
+func FuzzCalculateTex(f *testing.F) {
+	seed := []float64{-1.0, -2, 0, 500.0, 1000.0, 50000.0}
+
+	for _, v := range seed {
+		f.Add(v)
+	}
+
+	f.Fuzz(func(t *testing.T, amount float64) {
+		got := CalculateTax(amount)
+
+		if amount <= 0 && got != 0 {
+			t.Errorf("expected 0, got %f", got)
+		}
+
+		if amount > 20000 && got != 20 {
+			t.Errorf("expected 20, got %f", got)
+		}
+	})
+}

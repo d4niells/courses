53783724b6e5ad83cd2364d77a8edd65783040de
Daniel Oliveira <danieloliver.dev@gmail.com>
2024-05-02 12:36:03 -0300

    create product entity

---
+++ go-expert/api/internal/entity/product_test.go
@@ -0,0 +1,38 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProduct(t *testing.T) {
+
+	t.Run("create a valid product", func(t *testing.T) {
+		p, err := NewProduct("Product 1", 10)
+		assert.Nil(t, err)
+		assert.NotNil(t, p)
+		assert.NotEmpty(t, p.ID)
+		assert.Equal(t, "Product 1", p.Name)
+		assert.Equal(t, 10, p.Price)
+	})
+
+	t.Run("name is required", func(t *testing.T) {
+		p, err := NewProduct("", 10)
+		assert.Nil(t, p)
+		assert.EqualError(t, ErrNameIsRequired, err.Error())
+	})
+
+	t.Run("price is required", func(t *testing.T) {
+		p, err := NewProduct("Product 1", 0)
+		assert.Nil(t, p)
+		assert.EqualError(t, ErrPriceIsRequired, err.Error())
+	})
+
+	t.Run("price shouldn't less than zero", func(t *testing.T) {
+		p, err := NewProduct("Product 1", -10)
+		assert.Nil(t, p)
+		assert.EqualError(t, ErrInvalidPrice, err.Error())
+	})
+
+}

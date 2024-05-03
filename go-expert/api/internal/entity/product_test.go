a68bdfcaafb7d997fd18ff7b68b119cfccfd5365
Daniel Oliveira <danieloliver.dev@gmail.com>
2024-05-03 15:18:28 -0300

    create product db

--- go-expert/api/internal/entity/product_test.go
+++ go-expert/api/internal/entity/product_test.go
@@ -14,23 +14,23 @@ func TestProduct(t *testing.T) {
 		assert.NotNil(t, p)
 		assert.NotEmpty(t, p.ID)
 		assert.Equal(t, "Product 1", p.Name)
-		assert.Equal(t, 10, p.Price)
+		assert.Equal(t, 10.0, p.Price)
 	})
 
 	t.Run("name is required", func(t *testing.T) {
-		p, err := NewProduct("", 10)
+		p, err := NewProduct("", 10.0)
 		assert.Nil(t, p)
 		assert.EqualError(t, ErrNameIsRequired, err.Error())
 	})
 
 	t.Run("price is required", func(t *testing.T) {
-		p, err := NewProduct("Product 1", 0)
+		p, err := NewProduct("Product 1", 0.0)
 		assert.Nil(t, p)
 		assert.EqualError(t, ErrPriceIsRequired, err.Error())
 	})
 
 	t.Run("price shouldn't less than zero", func(t *testing.T) {
-		p, err := NewProduct("Product 1", -10)
+		p, err := NewProduct("Product 1", -10.0)
 		assert.Nil(t, p)
 		assert.EqualError(t, ErrInvalidPrice, err.Error())
 	})

f1f7f4a0bf1acab9b208e74a6637aa3a3aee0631
Daniel Oliveira <danieloliver.dev@gmail.com>
2024-05-15 10:06:32 -0300

    adding create user router on swagger

--- go-expert/api/internal/infra/webserver/handlers/user.go
+++ go-expert/api/internal/infra/webserver/handlers/user.go
@@ -11,6 +11,10 @@ import (
 	"github.com/go-chi/jwtauth"
 )
 
+type Error struct {
+	Message string `json:"message"`
+}
+
 type UserHandler struct {
 	UserDB        database.UserInterface
 	Jwt           *jwtauth.JWTAuth
@@ -63,6 +67,16 @@ func (h *UserHandler) Login(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(accessToken)
 }
 
+// Create user godoc
+// @Summary Create user
+// @Description Create user
+// @Tags users
+// @Accept json
+// @Produce json
+// @Param request body dto.CreateUserInput true "user request"
+// @Success 201
+// @Failure 500 {object} Error
+// @Router /users [post]
 func (h *UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var user dto.CreateUserInput
 	err := json.NewDecoder(r.Body).Decode(&user)
--- go-expert/api/pkg/entity/id.go
+++ go-expert/api/pkg/entity/id.go
@@ -7,6 +7,7 @@ type ID = uuid.UUID
 func NewID() ID {
 	return ID(uuid.New())
 }
+
 func ParseID(s string) (ID, error) {
 	id, err := uuid.Parse(s)
 	return ID(id), err

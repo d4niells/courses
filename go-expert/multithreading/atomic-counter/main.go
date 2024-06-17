f94d471f0fa590871b4d8f7386e026fef1bd5cb9
Daniel Oliveira <d4niells@duck.com>
2024-06-17 16:37:09 -0300

    solving concurrency problem using the atomic package

--- go-expert/multithreading/atomic-counter/main.go
+++ go-expert/multithreading/atomic-counter/main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	// "sync"
+	"sync/atomic"
 )
 
 var uniqueVisits uint64
 
 func main() {
-	mu := sync.Mutex{}
+	// mu := sync.Mutex{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		mu.Lock()
-		uniqueVisits++
-		mu.Unlock()
+		// mu.Lock()
+		atomic.AddUint64(&uniqueVisits, 1)
+		// mu.Unlock()
 		w.Write([]byte(fmt.Sprintf("You're visitor number %d", uniqueVisits)))
 	})
 

eb5f83292dcf8dafe71d408baa80494c66b6914b
Daniel Oliveira <d4niells@duck.com>
2024-06-17 16:33:23 -0300

    solving concurrency problem using mutex

--- go-expert/multithreading/atomic-counter/main.go
+++ go-expert/multithreading/atomic-counter/main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
 )
 
 var uniqueVisits uint64
 
 func main() {
+	mu := sync.Mutex{}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		mu.Lock()
 		uniqueVisits++
+		mu.Unlock()
 		w.Write([]byte(fmt.Sprintf("You're visitor number %d", uniqueVisits)))
 	})
 

4e38d305bcb02ad3d60117a878bc5880128e284f
Daniel Oliveira <d4niells@duck.com>
2024-06-19 02:12:12 -0300

    concurrently event publisher

--- go-expert/multithreading/channels/select/main.go
+++ go-expert/multithreading/channels/select/main.go
@@ -1,28 +1,48 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+type Message struct {
+	ID    int
+	Value string
+}
+
+func kafkaPub(ch chan Message) {
+	for {
+		time.Sleep(time.Second)
+		msg := Message{1, "Hello from kafka"}
+		ch <- msg
+	}
+}
+
+func rabbitMQPub(ch chan Message) {
+	for {
+		time.Sleep(2 * time.Second)
+		msg := Message{2, "Hello from rabbitMQ"}
+		ch <- msg
+	}
+}
 
 func main() {
-	ch1, ch2 := make(chan int), make(chan int)
+	ch1 := make(chan Message)
+	ch2 := make(chan Message)
 
-	go func() {
-		time.Sleep(1 * time.Second)
-		ch1 <- 1
-	}()
+	go kafkaPub(ch1)
+	go kafkaPub(ch2)
 
-	go func() {
-		time.Sleep(2 * time.Second)
-		ch2 <- 2
-	}()
-
-	select {
-	case msg1 := <-ch1:
-		println("received:", msg1)
-	case msg2 := <-ch2:
-		println("received:", msg2)
-	case <-time.After(3 * time.Second):
-		println("timeout")
-		// default: // We got default if all channels are empty
-		// 	println("default")
+	for {
+		select {
+		case msg := <-ch1:
+			fmt.Printf("Received from Kafka: %s\n", msg.Value)
+		case msg2 := <-ch2:
+			fmt.Printf("Received from RabbitMQ: %s\n", msg2.Value)
+		case <-time.After(3 * time.Second):
+			println("There're no messages to consume")
+			// default: // We got default if all channels are empty
+			// 	println("default")
+		}
 	}
 }

32f5564ade6033daf68acd6bbe9fee6414bc0b78
Daniel Oliveira <d4niells@duck.com>
2024-07-18 11:09:39 -0300

    create an event register

---
+++ go-expert/events/pkg/events/event_dispatcher.go
@@ -0,0 +1,26 @@
+package events
+
+import "errors"
+
+var ErrEventHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{handlers: make(map[string][]EventHandlerInterface)}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, exists := ed.handlers[eventName]; exists {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return ErrEventHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
---
+++ go-expert/events/pkg/events/event_dispatcher_test.go
@@ -0,0 +1,78 @@
+package events
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestEvent struct {
+	Name    string
+	Payload interface{}
+}
+
+func (e *TestEvent) GetName() string {
+	return e.Name
+}
+
+func (e *TestEvent) GetPayload() interface{} {
+	return e.Payload
+}
+
+func (e *TestEvent) GetDateTime() time.Time {
+	return time.Now()
+}
+
+type TestEventHandler struct {
+	ID int
+}
+
+func (h *TestEventHandler) Handle(event EventInterface) {}
+
+type EventDispatcherTestSuite struct {
+	suite.Suite
+	event           TestEvent
+	event2          TestEvent
+	handler         TestEventHandler
+	handler2        TestEventHandler
+	handler3        TestEventHandler
+	eventDispatcher *EventDispatcher
+}
+
+func (s *EventDispatcherTestSuite) SetupTest() {
+	s.eventDispatcher = NewEventDispatcher()
+	s.handler = TestEventHandler{ID: 1}
+	s.handler2 = TestEventHandler{ID: 2}
+	s.handler3 = TestEventHandler{ID: 3}
+	s.event = TestEvent{Name: "Test", Payload: "Test"}
+	s.event2 = TestEvent{Name: "Test", Payload: "Test"}
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.Nil(err)
+	suite.Equal(1, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler2)
+	suite.Nil(err)
+	suite.Equal(2, len(suite.eventDispatcher.handlers[suite.event.GetName()]))
+
+	assert.Equal(suite.T(), &suite.handler, suite.eventDispatcher.handlers[suite.event.GetName()][0])
+	assert.Equal(suite.T(), &suite.handler2, suite.eventDispatcher.handlers[suite.event.GetName()][1])
+}
+
+func (suite *EventDispatcherTestSuite) TestEventDispatcher_Register_WithSameHandler() {
+	err := suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.NoError(err)
+	suite.Len(suite.eventDispatcher.handlers[suite.event.GetName()], 1)
+
+	err = suite.eventDispatcher.Register(suite.event.GetName(), &suite.handler)
+	suite.EqualError(err, ErrEventHandlerAlreadyRegistered.Error())
+	suite.Len(suite.eventDispatcher.handlers[suite.event.GetName()], 1)
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(EventDispatcherTestSuite))
+}

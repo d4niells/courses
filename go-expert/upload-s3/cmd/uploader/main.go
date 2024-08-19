845443be851c337c9fcf6148fc8e9b12841638b7
Daniel Oliveira <d4niells@duck.com>
2024-08-19 19:30:41 -0300

    uploading files in parallel with a retry process when an error occurs

--- go-expert/upload-s3/cmd/uploader/main.go
+++ go-expert/upload-s3/cmd/uploader/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -16,6 +17,7 @@ var (
 	client *s3.S3
 	bucket string
 	tmpDir string
+	wg     sync.WaitGroup
 )
 
 func init() {
@@ -50,6 +52,20 @@ func main() {
 	}
 	defer dir.Close()
 
+	uploadControl, errorFileUpload := make(chan struct{}, 10000), make(chan string, 10)
+
+	go func() {
+		for {
+			select {
+			case filename := <-errorFileUpload:
+				fmt.Printf("Retrying to upload file %s\n", filename)
+				wg.Add(1)
+				uploadControl <- struct{}{}
+				go uploadFile(filename, uploadControl, errorFileUpload)
+			}
+		}
+	}()
+
 	for {
 		files, err := dir.ReadDir(1)
 		if err == io.EOF {
@@ -61,23 +77,38 @@ func main() {
 		}
 
 		for _, f := range files {
-			uploadFile(f.Name())
+			wg.Add(1)
+			uploadControl <- struct{}{}
+			go uploadFile(f.Name(), uploadControl, errorFileUpload)
 		}
 	}
+
+	defer wg.Wait()
 }
 
-func uploadFile(filename string) {
+func uploadFile(filename string, uploadControl <-chan struct{}, errorFileUpload chan<- string) {
+	defer wg.Done()
 	path := fmt.Sprintf("%s/%s", tmpDir, filename)
 	f, err := os.Open(path)
 	if err != nil {
 		fmt.Printf("Error opening file: %s\n", filename)
+		fmt.Println(err)
+		<-uploadControl
+		errorFileUpload <- filename
+		return
 	}
 	defer f.Close()
 
 	_, err = client.PutObject(&s3.PutObjectInput{Bucket: aws.String(bucket), Key: aws.String(filename), Body: f})
 	if err != nil {
 		fmt.Printf("Error uploading file: %s\n", filename)
+		fmt.Println(err)
+		<-uploadControl
+		errorFileUpload <- filename
+		return
+
 	}
 
 	fmt.Printf("File %s was uploaded successfully\n", filename)
+	<-uploadControl
 }
